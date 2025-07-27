8ab873b5887fc1db3752e6334be5eaa73c99a878
psaunderualberta <psaunder@ualberta.ca>
2025-07-27 16:59:49 -0600

    Added comments, accuracy computation

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from util import DATALOADERS
 from kan import KAN
 from jax import random as jr, numpy as jnp
-from util import non_vmap_cce_loss
+from util import non_vmap_cce_loss, subset_classification_accuracy
 import equinox as eqx
 from tqdm import tqdm
 import optax
@@ -10,29 +10,37 @@ import optax
 def main():
     # Load data
     x, y = DATALOADERS["mnist"]()
+    key = jr.PRNGKey(0)
+    key, _key = jr.split(key)
 
+    # Initialize network
     dims = [x.shape[1], 32, y.shape[1]]
+    network = KAN(dims, 7, 3, 3, _key)
 
-    network = KAN(dims, 7, 3, 3, jr.PRNGKey(0))
-
-    dataset_idxs = jnp.arange(x.shape[0])
-    shuffle_key = jr.PRNGKey(0)
-
+    # optimizer
     batch_size = 512
-    batch_x, batch_y = x[:32, :], y[:32, :]
-    optimizer = optax.sgd(learning_rate=0.1)
+    optimizer = optax.adam(0.1)
     opt_state = optimizer.init(network)
 
+    # Create visual indicator of progress
     iterator = tqdm(range(1000), total=1000)
-
     for iteration in iterator:
-        shuffle_key, _key = jr.split(shuffle_key)
+        # Get batch, note that we don't
+        # iterate over the entire dataset then shuffle.
+        # Easier to implement this way :P
+        key, _key = jr.split(key)
         idxs = jr.permutation(_key, x.shape[0])[:batch_size]
-
         batch_x, batch_y = x[idxs, :], y[idxs, :]
 
+        # Compute loss, accuracy, gradients
+        key, _key = jr.split(key)
         loss, grads = non_vmap_cce_loss(network, batch_x, batch_y)
-        iterator.set_description(f"{iteration} | {loss:.4f}")
+        accuracy = subset_classification_accuracy(network, x, y, 0.01, _key)
+
+        # Update iterator description
+        iterator.set_description(f"{iteration} | {loss:.4f} | {accuracy:.2f}")
+
+        # Update network
         updates, opt_state = optimizer.update(grads, opt_state, network)
         network = eqx.apply_updates(network, updates)  # None-safe
 
--- src/util/losses.py
+++ src/util/losses.py
@@ -1,5 +1,6 @@
 import equinox as eqx
 from optax import softmax_cross_entropy
+from jax import vmap, numpy as jnp, random as jr
 import chex
 
 
@@ -9,3 +10,17 @@ def non_vmap_cce_loss(model: eqx.Module, x: chex.Array, y: chex.Array):
     pred_y = model(x).squeeze()
 
     return softmax_cross_entropy(pred_y, y).mean()
+
+@eqx.filter_jit
+def classification_accuracy(model, x, y):
+    pred_y = model(x)
+    pred_y_int = jnp.argmax(pred_y, axis=1)
+    y_int = jnp.argmax(y, axis=1)
+    return jnp.mean(pred_y_int == y_int) * 100
+
+
+def subset_classification_accuracy(model, x, y, percent, key):
+    num_samples = x.shape[0]
+    num_samples = int(num_samples * percent)
+    idxs = jr.permutation(key, jnp.arange(x.shape[0]))[:num_samples]
+    return classification_accuracy(model, x[idxs], y[idxs])
