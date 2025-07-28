e38e9af340aac2b55424db24a0f298995a276540
psaunderualberta <psaunder@ualberta.ca>
2025-07-28 13:45:07 -0600

    Added non-linearity to MLP

--- src/mlp/mlp.py
+++ src/mlp/mlp.py
@@ -1,13 +1,14 @@
 import equinox as eqx
 import jax.random as jr
+from util import SILU
 import chex
 
 
 class MLP(eqx.Module):
-    def __init__(self, dims: chex.Array, key: chex.PRNGKey):
+    def __init__(self, dims: chex.Array, key: chex.PRNGKey, activation: eqx.Module = SILU):
         in_dim = dims[0]
         self.layers = []
-        for out_dim in dims[1:]:
+        for out_dim in dims[1:-1]:
             key, _key = jr.split(key)
             self.layers.append(
                 eqx.nn.Linear(
@@ -17,8 +18,14 @@ class MLP(eqx.Module):
                 )
             )
 
+            self.layers.append(activation)
+
             # move to next layer
             in_dim = out_dim
+        
+        self.layers.append(
+            eqx.nn.Linear(in_dim, dims[-1], key=key)
+        )
 
     def __call__(self, x):
         for layer in self.layers:
