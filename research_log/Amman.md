d7a7463485dde68ce610320d5114bad4ff779080
ammanyusuf <ammanyusuf85@gmail.com>
2025-07-29 13:17:03 -0700

    feat: add KAN to basic Q-streaming

--- research_log/Amman.md
+++ research_log/Amman.md
@@ -1,2 +1,7 @@
 # Amman's Research Log
 ### July 28th, 2025
+- get familiar with code base, RL implementations
+
+### July 29th, 2025
+- implement MLP RL with basic Q-streaming, get model to learn
+- add KAN network to basic Q-streaming
--- src/streaming_agents.py
+++ src/streaming_agents.py
@@ -4,6 +4,7 @@ import equinox as eqx
 from jax import random as jr, numpy as jnp, value_and_grad
 from tqdm import tqdm
 from util.losses import q_epsilon_greedy, q_td_error, q_huber_loss
+from util import linear_epsilon_schedule
 from mlp.mlp import MLP
 from kan.kan import KAN
 from gymnax import make
@@ -15,6 +16,10 @@ class StreamingAgent:
         self.env, self.env_params = make(env_name)
         self.env_name = env_name
         self.name = "BaseAgent"
+        self.episode_count = 0
+        self.total_steps = 0
+        self.total_rewards = []
+        self.avg_td_errors = []
     
     def select_action(self, state):
         """Select an action given the current state."""
@@ -63,15 +68,15 @@ class StreamingAgent:
             'avg_td_error': sum(episode_td_errors) / (step + 1)
         }
     
-    def run(self, num_episodes=10, max_steps_per_episode=1000, verbose=True):
-        """Run the agent for multiple episodes."""
+    def run(self, num_episodes=10, max_steps_per_episode=1000, verbose=True, save_plot=True, plot_path="training_plot.png"):
+        """Run the agent for multiple episodes and save a plot of rewards and TD errors."""
+        import matplotlib.pyplot as plt
         if verbose:
             episodes_iter = tqdm(range(num_episodes), desc=f"Training {self.name}")
         else:
             episodes_iter = range(num_episodes)
-        
+
         episode_results = []
-        
         for episode in episodes_iter:
             result = self.run_episode(max_steps=max_steps_per_episode)
             episode_results.append(result)
@@ -81,7 +86,6 @@ class StreamingAgent:
                 avg_reward = sum(recent_rewards) / len(recent_rewards)
                 recent_td_errors = [r['avg_td_error'] for r in episode_results[-10:]]
                 avg_td_error = sum(recent_td_errors) / len(recent_td_errors)
-                
                 episodes_iter.set_description(
                     f"{self.name} | "
                     f"Episode {episode+1}/{num_episodes} | "
@@ -89,7 +93,13 @@ class StreamingAgent:
                     f"10-Ep Avg: {avg_reward:.2f} | "
                     f"TD Error: {avg_td_error:.4f}"
                 )
-        
+
+        if save_plot:
+            from util.plotting import plot_training_progress
+            rewards = [r['total_reward'] for r in episode_results]
+            td_errors = [r['avg_td_error'] for r in episode_results]
+            plot_training_progress(rewards, td_errors, plot_path=f"{self.name}_{plot_path}", agent_name=self.name)
+
         return episode_results
 
 class NetworkStreamQAgent(StreamingAgent):
@@ -102,14 +112,19 @@ class NetworkStreamQAgent(StreamingAgent):
         env_name="CartPole-v1",
         learning_rate=0.001,
         discount_factor=0.99,
-        epsilon=0.1,
+        start_e=1.0,
+        end_e=0.01,
+        decay_duration=500,
         seed=0
     ):
         super().__init__(env_name)
         self.name = "NetworkQAgent"
         self.alpha = learning_rate
         self.gamma = discount_factor
-        self.epsilon = epsilon
+        self.start_e = start_e
+        self.end_e = end_e
+        self.decay_duration = decay_duration
+
         self.key = jr.PRNGKey(seed)
         
         obs_space = self.env.observation_space(self.env_params)
@@ -121,21 +136,22 @@ class NetworkStreamQAgent(StreamingAgent):
         # optimizer
         self.optimizer = optax.adam(learning_rate)
         self.opt_state = self.optimizer.init(self.network)
-        
-        self.episode_count = 0
-        self.total_steps = 0
-        self.total_rewards = []
-        self.avg_td_errors = []
     
     def select_action(self, state):
         """Epsilon-greedy action selection using utility function."""
         self.key, subkey = jr.split(self.key)
         
         state_array = jnp.array(state)
+        eps = linear_epsilon_schedule(
+            self.start_e, 
+            self.end_e, 
+            self.decay_duration, 
+            self.episode_count
+        )
         action, _, _ = q_epsilon_greedy(
             self.network, 
             state_array, 
-            self.epsilon, 
+            eps,
             subkey
         )
         
@@ -174,7 +190,9 @@ class MLPStreamQAgent(NetworkStreamQAgent):
         hidden_dims=[64, 32],
         learning_rate=0.001,
         discount_factor=0.99,
-        epsilon=0.1,
+        start_e=1.0,
+        end_e=0.01,
+        decay_duration=500,
         seed=0
     ):
         env, env_params = make(env_name)
@@ -197,8 +215,57 @@ class MLPStreamQAgent(NetworkStreamQAgent):
             env_name,
             learning_rate,
             discount_factor,
-            epsilon,
+            start_e,
+            end_e,
+            decay_duration,
             seed
         )
         
         self.name = "MLPQAgent"
+
+
+class KANStreamQAgent(NetworkStreamQAgent):
+    """
+    Streaming Q-learning agent with KAN.
+    """
+    def __init__(
+        self,
+        env_name="CartPole-v1",
+        hidden_dims=[64, 32],
+        learning_rate=0.001,
+        discount_factor=0.99,
+        start_e=1.0,
+        end_e=0.01,
+        decay_duration=500,
+        grid=7,
+        k=3, 
+        num_stds=3,
+        seed=0
+    ):
+        env, env_params = make(env_name)
+        obs_space = env.observation_space(env_params)
+        obs_dim = len(obs_space.shape)
+        if obs_dim == 0:
+            obs_dim = 1
+        else:
+            obs_dim = obs_space.shape[0]
+        
+        num_actions = env.num_actions
+        
+        key = jr.PRNGKey(seed)
+        key, subkey = jr.split(key)
+        network_dims = [obs_dim] + hidden_dims + [num_actions]
+        network = KAN(network_dims, grid, k, num_stds, subkey)
+        
+        super().__init__(
+            network,
+            env_name,
+            learning_rate,
+            discount_factor,
+            start_e,
+            end_e,
+            decay_duration,
+            seed
+        )
+        
+        self.name = "KANQAgent"
